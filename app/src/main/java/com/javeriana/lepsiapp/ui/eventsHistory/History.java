bdb01b7accedd893b9b3fc7f0bad58a0008e3c64
dm-rodriguez <99518816+dm-rodriguez@users.noreply.github.com>
2022-05-02 15:07:49 -0500

    Update
    Update

--- app/src/main/java/com/javeriana/lepsiapp/ui/eventsHistory/History.java
+++ app/src/main/java/com/javeriana/lepsiapp/ui/eventsHistory/History.java
@@ -2,6 +2,7 @@ package com.javeriana.lepsiapp.ui.eventsHistory;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.database.Cursor;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.javeriana.lepsiapp.R;
@@ -20,12 +22,13 @@ import com.javeriana.lepsiapp.ui.dashboard.DashboardViewModel;
 
 public class History extends Fragment {
 
-    private FragmentHistoryBinding binding;
 
+    private FragmentHistoryBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HistoryViewModel historyViewModel = new ViewModelProvider(this).get(HistoryViewModel.class);
+
 
         binding = FragmentHistoryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
